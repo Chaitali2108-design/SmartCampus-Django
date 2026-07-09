a258f5b091bfd5da63736ccf0642a8ff2f1a6615
Chaitali Chandele <chaitalichandele2108@gmail.com>
2026-07-09 16:01:55 +0530

    add submit api in api views

--- dashboard/api/views.py
+++ dashboard/api/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 
 from .executor import CodeExecutor
+from dashboard.models import CodingQuestion
 
 
 @csrf_exempt
@@ -40,18 +41,90 @@ def run_code_api(request):
         )
 
 
+from dashboard.models import CodingQuestion
+import json
+
+
 @csrf_exempt
 def submit_code_api(request):
 
     if request.method != "POST":
         return JsonResponse(
-            {"error": "Only POST requests are allowed."},
+            {"success": False, "error": "POST request required."},
             status=405
         )
 
-    data = json.loads(request.body)
+    try:
 
-    return JsonResponse({
-        "success": True,
-        "message": "Submit API will be implemented in the next stage."
-    })
+        data = json.loads(request.body)
+
+        question_id = data.get("question_id")
+        language = data.get("language")
+        code = data.get("code")
+
+        question = CodingQuestion.objects.get(id=question_id)
+
+        result = CodeExecutor.execute(
+            language=language,
+            code=code,
+            custom_input=question.official_input
+        )
+
+        if not result["success"]:
+            return JsonResponse({
+                "success": False,
+                "verdict": "Runtime Error",
+                "expected": question.expected_output,
+                "received": result["error"]
+            })
+
+        expected = question.expected_output.strip()
+        received = result["output"].strip()
+
+        if expected == received:
+
+            return JsonResponse({
+
+                "success": True,
+
+                "verdict": "Accepted",
+
+                "marks": question.marks,
+
+                "total_marks": question.marks
+
+            })
+
+        return JsonResponse({
+
+            "success": False,
+
+            "verdict": "Wrong Answer",
+
+            "expected": expected,
+
+            "received": received
+
+        })
+
+    except CodingQuestion.DoesNotExist:
+
+        return JsonResponse({
+
+            "success": False,
+
+            "verdict": "Question Not Found"
+
+        })
+
+    except Exception as e:
+
+        return JsonResponse({
+
+            "success": False,
+
+            "verdict": "Server Error",
+
+            "received": str(e)
+
+        })
